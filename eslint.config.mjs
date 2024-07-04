dd4a7b0441958631cc3ef4197f5d50c22e54ef14
tanbir-hossen <tanbirhosse911@gmail.com>
2024-07-04 16:11:44 +0600

    basic project setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["node_module", "dist"]
+}
+];
